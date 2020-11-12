7ec35bfe55d30ff065291583588b21d5398760bd
Bruno Barbosa <barbosa.developer@gmail.com>
2020-11-11 21:16:34 -0300

    change port node execute

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -37,6 +37,6 @@ app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
     .json({ status: 'error', message: 'Internal Server Error' });
 });
 
-app.listen(3333, () => {
-  console.log('Server stated on port 3333!');
+app.listen(3434, () => {
+  console.log('Server stated on port 3434!');
 });

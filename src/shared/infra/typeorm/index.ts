fb09657337736d124f02bd6e1294160d4ac514e7
Bruno Barbosa <barbosa.developer@gmail.com>
2020-11-12 00:44:58 -0300

    ajust to find db conections config

--- src/shared/infra/typeorm/index.ts
+++ src/shared/infra/typeorm/index.ts
@@ -1,27 +1,3 @@
 import { createConnections } from 'typeorm';
 
-createConnections([
-  {
-    name: 'default',
-    type: 'postgres',
-    host: '192.168.99.100',
-    port: 5433,
-    username: 'postgres',
-    password: 'postgres',
-    database: 'bdm_brasucas',
-    entities: ['./src/modules/**/infra/typeorm/entities/*.ts'],
-    migrations: ['./src/shared/infra/typeorm/migrations/*.ts'],
-    cli: {
-      migrationsDir: './src/shared/infra/typeorm/migrations',
-    },
-  },
-  {
-    name: 'mongo',
-    type: 'mongodb',
-    host: '192.168.99.100',
-    port: 27017,
-    database: 'bdm_brasucas',
-    useUnifiedTopology: true,
-    entities: ['./src/modules/**/infra/typeorm/schemas/*.ts'],
-  },
-]);
+createConnections();
